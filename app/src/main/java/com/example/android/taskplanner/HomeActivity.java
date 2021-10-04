4602fe2adc125d7fd8eccda67fa280d57782e389
Rohan <rohanbondre2001@gmail.com>
2021-10-04 22:03:22 +0530

    creating home page

--- app/src/main/java/com/example/android/taskplanner/HomeActivity.java
+++ app/src/main/java/com/example/android/taskplanner/HomeActivity.java
@@ -2,12 +2,26 @@ package com.example.android.taskplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        textView = findViewById(R.id.current_date);
+
+        Calendar calender = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d");
+        String dateformate = simpleDateFormat.format(calender.getTime());
+        textView.setText(dateformate);
+
     }
 }
