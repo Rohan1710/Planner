98845243e6c5dd65c5b0ab636c0048265251e3e6
Rohan <rohanbondre2001@gmail.com>
2021-10-05 16:54:03 +0530

    creating home page

--- app/src/main/java/com/example/android/taskplanner/HomeActivity.java
+++ app/src/main/java/com/example/android/taskplanner/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.taskplanner;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -10,6 +14,7 @@ import java.util.Calendar;
 public class HomeActivity extends AppCompatActivity {
 
     TextView textView;
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +27,27 @@ public class HomeActivity extends AppCompatActivity {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEE, MMM d");
         String dateformate = simpleDateFormat.format(calender.getTime());
         textView.setText(dateformate);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId())
+                {
+                    case R.id.home:
+                        Toast.makeText(HomeActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    case R.id.todolist:
+                        Toast.makeText(HomeActivity.this, "todo list", Toast.LENGTH_SHORT).show();
+                    case R.id.notes:
+                        Toast.makeText(HomeActivity.this, "Notes", Toast.LENGTH_SHORT).show();
+                    case R.id.alarm:
+                        Toast.makeText(HomeActivity.this, "alarm", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
     }
 }
